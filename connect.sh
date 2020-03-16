f9f0fc4624d99c42b95540341e2fc102e304eece
Critter Johnson <critterjohnson45@gmail.com>
2020-03-16 18:38:27 -0400

    more scripts and updated scripts

--- connect.sh
+++ connect.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-export IMAGE_FAMILY="pytorch-latest-gpu" # or "pytorch-latest-cpu" for non-GPU instances
-export ZONE="us-west1-b"
-export INSTANCE_NAME="my-fastai-instance"
-export INSTANCE_TYPE="n1-highmem-8" # budget: "n1-highmem-4"
+export ZONE="us-east1-b"
+export INSTANCE_NAME="fastai-instance"
 
 gcloud compute ssh --zone=$ZONE jupyter@$INSTANCE_NAME -- -L 8080:localhost:8080
--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export ZONE="us-west1-b"
-export INSTANCE_NAME="my-fastai-instance"
+export ZONE="us-east1-b"
+export INSTANCE_NAME="fastai-instance"
 
 gcloud compute instances start $INSTANCE_NAME --zone $ZONE
